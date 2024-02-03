9d41933fe182888103e9212b4f86f4dd975457bc
andrewsga89 <andrews.ga89@gmail.com>
2024-02-03 16:39:37 -0300

    Inclusão gameClicado

--- js/app.js
+++ js/app.js
@@ -3,6 +3,9 @@ function alterarStatus(id){
     let image = gameClicado.querySelector('.dashboard__item__img');
     let botao = gameClicado.querySelector('.dashboard__item__button');
     let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
+    
+    botao = gameClicado();
+    document.getElementById('game-3').setAttribute('dashboard__item__button dashboard__item__button--return', true)
 
     alert(nomeJogo.textContent);
 }
