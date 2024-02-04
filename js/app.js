a29e22d088ea028eb38159cb716ad07070fcb1b6
andrewsga89 <andrews.ga89@gmail.com>
2024-02-04 00:37:41 -0300

    Incluão botão .js

--- js/app.js
+++ js/app.js
@@ -2,10 +2,10 @@ function alterarStatus(id){
     let gameClicado = document.getElementById(`game-${id}`);
     let image = gameClicado.querySelector('.dashboard__item__img');
     let botao = gameClicado.querySelector('.dashboard__item__button');
-    let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
     
-    botao = gameClicado();
-    document.getElementById('game-3').setAttribute('dashboard__item__button dashboard__item__button--return', true)
+    if (image.classList.contains('dashboard__item__img--rented')) {
 
-    alert(nomeJogo.textContent);
+    } else {
+
+    }
 }
